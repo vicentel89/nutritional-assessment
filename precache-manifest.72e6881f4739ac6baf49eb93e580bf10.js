359bdf33b9659f9b89a08c4a2fd8a04bb0910d81
vicentel89 <60248266+vicentel89@users.noreply.github.com>
2020-07-04 17:06:28 -0500

    Final

---
+++ precache-manifest.72e6881f4739ac6baf49eb93e580bf10.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef3cd2046192c8588c1a94f92ec61bf",
+    "url": "/nutritional-assessment/index.html"
+  },
+  {
+    "revision": "4e21e2cac6137bd059d2",
+    "url": "/nutritional-assessment/static/css/3.533756cf.chunk.css"
+  },
+  {
+    "revision": "cda87f0068aaf622fefb",
+    "url": "/nutritional-assessment/static/css/main.b619e870.chunk.css"
+  },
+  {
+    "revision": "7ff5c681c2f59c9b429e",
+    "url": "/nutritional-assessment/static/js/2.038ba49e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nutritional-assessment/static/js/2.038ba49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e21e2cac6137bd059d2",
+    "url": "/nutritional-assessment/static/js/3.077a8bfb.chunk.js"
+  },
+  {
+    "revision": "cda87f0068aaf622fefb",
+    "url": "/nutritional-assessment/static/js/main.f04306b8.chunk.js"
+  },
+  {
+    "revision": "2f27a30ded8edc19ff32",
+    "url": "/nutritional-assessment/static/js/runtime-main.24fa380a.js"
+  },
+  {
+    "revision": "5bd47d7ed0273fbc5661a09b80d818a9",
+    "url": "/nutritional-assessment/static/media/BMIInfoGraphRail.5bd47d7e.svg"
+  },
+  {
+    "revision": "ae4689bd5cd024c016023dedf1a9119b",
+    "url": "/nutritional-assessment/static/media/BMIResultIcon.ae4689bd.svg"
+  },
+  {
+    "revision": "1bf8cfef94262027bacb57e7940056b2",
+    "url": "/nutritional-assessment/static/media/calorieExpenditureIcon.1bf8cfef.svg"
+  },
+  {
+    "revision": "6b52c4e7fc1d70b9387df2d798b45d2c",
+    "url": "/nutritional-assessment/static/media/calorieExpenditureMeter.6b52c4e7.svg"
+  },
+  {
+    "revision": "47f63ea1b22343e0a7ce062ad9faa57a",
+    "url": "/nutritional-assessment/static/media/logo.47f63ea1.svg"
+  },
+  {
+    "revision": "f3cd90a6345f47e6447164bc5d544e44",
+    "url": "/nutritional-assessment/static/media/weightRangeIcon.f3cd90a6.svg"
+  },
+  {
+    "revision": "50cb923b5a7ab2c300f4f889fc6047ac",
+    "url": "/nutritional-assessment/static/media/weightRangeInfoGraphRail.50cb923b.svg"
+  },
+  {
+    "revision": "1653462283e653fe26f82491e4695923",
+    "url": "/nutritional-assessment/static/media/weightRangeInfoGraphThumbNormal.16534622.svg"
+  },
+  {
+    "revision": "46f78c921611b62df4736c12a6e679e7",
+    "url": "/nutritional-assessment/static/media/weightRangeInfoGraphThumbOver.46f78c92.svg"
+  },
+  {
+    "revision": "eef3cdcaf3ef880455cbc8dca7a225d2",
+    "url": "/nutritional-assessment/static/media/weightRangeInfoGraphThumbOverweight.eef3cdca.svg"
+  },
+  {
+    "revision": "14dc16948c2ab284537e0f9d617b0ef8",
+    "url": "/nutritional-assessment/static/media/weightRangeInfoGraphThumbUnder.14dc1694.svg"
+  },
+  {
+    "revision": "3850a0448ca8b3c5e311c17233f3f174",
+    "url": "/nutritional-assessment/static/media/weightResultIcon.3850a044.svg"
+  },
+  {
+    "revision": "5d1713ea1ac99a06641809910f6881d0",
+    "url": "/nutritional-assessment/static/media/weightResultInfoGraphNormal.5d1713ea.svg"
+  },
+  {
+    "revision": "b084ce1c8127dfdf1c9bbe0d41a5a95f",
+    "url": "/nutritional-assessment/static/media/weightResultInfoGraphOver.b084ce1c.svg"
+  },
+  {
+    "revision": "5b4efd30f355142cb33f9d8f1f8717a0",
+    "url": "/nutritional-assessment/static/media/weightResultInfoGraphUnder.5b4efd30.svg"
+  }
+]);
